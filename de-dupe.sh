871e264a0345ab7062f2b3a61fb90e3c867e828b
Matt Cooper <m.cooper44@gmail.com>
2024-03-16 21:47:33 -0400

    removes dupe lines by key

--- de-dupe.sh
+++ de-dupe.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# https://unix.stackexchange.com/questions/30173/how-to-remove-duplicate-lines-inside-a-text-file
-awk -i inplace '!seen[$0]++' housing_list.csv
+#https://unix.stackexchange.com/questions/171091/remove-lines-based-on-duplicates-within-one-column-without-sort
+awk -i inplace -F',' '!seen[$1]++' housing_list.csv
