de9eaf180539949974a465b3a454651e1429b11c
Matt Cooper <m.cooper44@gmail.com>
2024-03-21 22:33:52 -0400

    basic funcs for address parsing

---
+++ util.py
@@ -0,0 +1,31 @@
+from ez_address_parser import AddressParser
+
+
+def process_ap(lst, lbl, max_rep=2):
+    r = []
+    for t in lst:
+        v,l = t
+        if l == lbl:
+            if len(r) < max_rep:
+                r.append(v)
+    return ' '.join(r) if r else None
+
+def get_sa(lst):
+    ls = ['StreetNumber', 'StreetName', 'StreetType',
+          'StreetDirection']
+    s = []
+    for v in ls:
+        t = process_ap(lst, v)
+        if t:
+            s.append(t)
+    return ' '.join(s) if s else None
+
+
+
+def parse_address(address):
+    ap = AddressParser()
+    t = ap.parse(address)
+    city = process_ap(t, 'Municipality', 1)
+    pcode = process_ap(t, 'PostalCode')
+    street = get_sa(t)
+
