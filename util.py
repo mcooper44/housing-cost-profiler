dc4813dc050ea9e570d6483c76122b4e6ca7e714
Matt Cooper <m.cooper44@gmail.com>
2024-03-25 22:42:45 -0400

    expanded doc str, made proc appliance func generic
    the process_appliance function could be generally
    applied to several of the values in the h4 data structure
    so renamed and documented change
    
    generally expanded doc strings here to map where
    the data is coming from and validated them via
    some values in the repl

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ from typing import Union
 def process_ap(lst: list, lbl: str, max_rep: int=2):
     '''
     take an ez-address-parser list and a specific
-    label and return a string made of up thos elements
+    label and return a string made of up those elements
     e.g. postal code, municipality
     '''
     r = []
@@ -17,6 +17,7 @@ def process_ap(lst: list, lbl: str, max_rep: int=2):
 
 def get_sa(lst: list) -> Union[str, None]:
     '''
+    get street address
     call process_ap with a list of street
     address labels to recompose the street address
     into a distinct string for logging in the dbase
@@ -35,18 +36,19 @@ def get_sa(lst: list) -> Union[str, None]:
 def process_address(address: list) -> tuple:
     '''
     leverage ez-address-parser to process the address string
-    into labelled element, label pairs
+    into labelled (element, label pairs)
 
     call the helper functions process_ap and get_sa to separate
     out the elements needed for the database
 
     return a list of (street string, city string, postal code string)
+    to facilitate db insertion
     '''
     ap = AddressParser()
     t = ap.parse(address)
     city = process_ap(t, 'Municipality', 1)
     pcode = process_ap(t, 'PostalCode')
-    prov = process_ap(t, 'Province')
+    prov = process_ap(t, 'Province', 1)
     street = get_sa(t)
     return (street, city, prov, pcode)
 
@@ -56,6 +58,15 @@ def process_utility(utility: list) -> tuple:
     recieves the 'Utilities Included'
     output from a_listing method which is a list
     and returns a dictionary keyed by utility
+    Utilities Included is in the h4 tags in the listing
+    which returns a list
+    an example of input is:
+    ['No: Hydro', 'Yes: Heat', 'Yes: Water','HydroHeatWater']
+    as the function lumps the headings into one str we want
+    to avoid the last value
+    and convert the yes' to 1 and no to 0 and errors to -1
+    returns a tuple so that these values can be integrated
+    into a db insertion
     '''
     d = {}
     lu = {'No': 0, 'Yes': 1}
@@ -65,11 +76,32 @@ def process_utility(utility: list) -> tuple:
             d[v[1].strip()] = lu.get(v[0], -1)
     return (d.get('Hydro', -1), d.get('Water', -1), d.get('Heat', -1))
 
-def process_appliance(LID: int, apps: list) -> tuple:
+def process_item_list(LID: int, apps: list) -> tuple:
+    '''
+    USED TO PROCESS
+    -Amenities,
+    -Appliances
+    -Personal Outdoor Space
+    All are in  h4 tags, and can be a variety
+    of items stored in a ul or 'Not Included'
+    the parsing function drops them into a list
+    so this function pairs them with the LID
+    in a tuple for easier insertion to the db
+    '''
     return [(LID, app) for app in apps]
 
 def process_price(p):
+    '''
+    Typically formatted $X,XXX or 'Please Contact'
+    So try to extract the number formatting and return
+    an integer.
+    Price is in the data stripped out of the title
+    '''
+    if p == 'Please Contact':
+        return -1
     try:
         return int(p.translate(str.maketrans('', '', '$,')))
     except:
         return -1
+
+
